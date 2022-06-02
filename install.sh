5845e31c8aff0599c5de1aa96ab1091484ac0ce6
pSchlarb <p.schlarb@esatus.com>
2022-06-02 10:06:25 +0000

    copy zsh
    Signed-off-by: pSchlarb <p.schlarb@esatus.com>

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+rm ~/.zshrc 
+ln -s ~/.dotfiles/.zshrc ~/
+ln -s ~/.dotfiles/.p10k.zsh ~/
